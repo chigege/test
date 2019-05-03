2c0df72df32f3adb362207955205ef21434fa747
guochi <763310673@qq.com>
2019-05-03 16:50:56 +0800

    guochi

---
+++ ssm/src/main/java/com/own/ssm/service/PaperService.java
@@ -0,0 +1,18 @@
+package com.own.ssm.service;
+
+
+import com.own.ssm.mode.*;
+
+import java.util.List;
+
+public interface PaperService {
+    int addPaper(Paper paper);
+
+    int deletePaperById(long id);
+
+    int updatePaper(Paper paper);
+
+    Paper queryById(long id);
+
+    List<Paper> queryAllPaper();
+}
---
+++ ssm/src/main/java/com/own/ssm/service/PaperServiceImpl.java
@@ -0,0 +1,40 @@
+package com.own.ssm.service;
+import com.own.ssm.dao.PaperDao;
+import com.own.ssm.mode.Paper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaperServiceImpl implements PaperService {
+    @Autowired
+    private PaperDao paperDao;
+
+    @Override
+    public int addPaper(Paper paper) {
+        return paperDao.addPaper(paper);
+    }
+
+    @Override
+    public int deletePaperById(long id) {
+        return paperDao.deletePaperById(id);
+    }
+
+    @Override
+    public int updatePaper(Paper paper) {
+        return paperDao.updatePaper(paper);
+    }
+
+    @Override
+    public Paper queryById(long id) {
+        return paperDao.queryById(id);
+    }
+
+    @Override
+    public List<Paper> queryAllPaper() {
+        return paperDao.queryAllPaper();
+    }
+
+}
+
